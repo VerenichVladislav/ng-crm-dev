78ce8916d9a5f2dd20c40334f30930ab2190e09b
Egor Kamysh <egorkamysh99@gmail.com>
2019-11-20 01:45:48 +0300

    buyTicket is ready!

--- src/app/components/buy-ticket/buy-ticket.component.ts
+++ src/app/components/buy-ticket/buy-ticket.component.ts
@@ -1,10 +1,11 @@
-import {Component, Injectable, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormArray, FormBuilder} from '@angular/forms';
-import { Trip } from 'src/app/entity/trip';
-import { TripService } from 'src/app/shared/trip.service';
-
-
+import {Component, Injectable, Input, OnInit} from '@angular/core';
+import {TripService} from '../../shared/trip.service';
+import {Trip} from '../../entity/trip';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder} from '@angular/forms';
+import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {TicketService} from '../../shared/ticket.service';
+import { parseHttpResponse } from 'selenium-webdriver/http';
 @Component({
   selector: 'app-buy-ticket',
   templateUrl: './buy-ticket.component.html',
@@ -13,51 +14,72 @@ import { TripService } from 'src/app/shared/trip.service';
 @Injectable({
   providedIn: 'root'
 })
-
 export class BuyTicketComponent implements OnInit {
-  appTitle = 'Информация о рейсе';
-  trip:Trip;
+  appTitle = 'Flight Information';
+  trip: Trip;
   count: number;
-
-  ticketForm: FormGroup;
-  passengers: FormArray;
-
+  public usersForm: FormGroup;
+  error: HttpErrorResponse;
+  idT:number;
+  idU:number;
+  message: any;
   constructor(private tripService: TripService,
               private route: ActivatedRoute,
-              private formBuilder: FormBuilder){
-    this.ticketForm = formBuilder.group({
-
-      passengers: this.formBuilder.array([ this.createPsg() ])
-    });
-  }
-
-  createPsg(): FormGroup {
-    return this.formBuilder.group({
-      name: '',
-      lastName: ''
-    });
-  }
-  addPsg(): void {
-    this.passengers = this.ticketForm.get('passengers') as FormArray;
-    this.passengers.push(this.createPsg());
-  }
-  submit(){
-    console.log(this.ticketForm);
-  }
+              private router: Router,
+              private fb: FormBuilder,
+              private ticketService: TicketService,
+              private http: HttpClient){}
 
     ngOnInit() {
     const tripid = this.route.snapshot.paramMap.get('tripid');
-
+    this.idT = parseInt(tripid);
+    const userid = this.route.snapshot.paramMap.get('userid');
+    this.idU = parseInt(userid);
     this.trip = this.tripService.getTrip(tripid).subscribe(
         data => this.trip = data
        );
     this.route.queryParams.subscribe(params => {
-        this.count = parseInt(params.count);
-        console.log(this.count);
+        this.count = parseInt(params.count);  
       });
-    this.ticketForm = this.formBuilder.group({
-        passengers: this.formBuilder.array([ this.createPsg() ])
-      });
-    console.log(this.passengers);
+  
+    this.usersForm = this.fb.group({
+      users: this.fb.array([
+        this.fb.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required]
+        })
+      ])
+    })
+    this.addFormControl();
+    this.usersForm.valueChanges.subscribe((value)=>console.log(value));
   }
-}
+
+  removeFormControl(i) {
+    let usersArray = this.usersForm.controls.users as FormArray;
+    usersArray.removeAt(i);
+  }
+
+  addFormControl() {
+    for (let i = 1; i < this.count; i++) {
+      let usersArray = this.usersForm.controls.users as FormArray;
+      let arraylen = usersArray.length;
+      let newUsergroup: FormGroup = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required]
+      })
+      usersArray.insert(arraylen, newUsergroup);
+    }
+  }
+
+  submit(){
+    this.ticketService.submitForm(this.usersForm.controls.users.value, this.count, this.idT, this.idU)
+                .subscribe(
+                    (log: HttpErrorResponse) => {      
+                        this.message = log;     
+                    },
+                    (error) => {
+                      this.message = error.error;
+                    }
+                );
+        }
+ }
--- src/app/shared/ticket.service.ts
+++ src/app/shared/ticket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {GlobalRootURL} from '../GlobalRootURL';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import {User} from '../entity/user';
 import {Observable} from 'rxjs';
 import {Ticket} from '../entity/ticket';
@@ -17,8 +17,14 @@ export class TicketService {
     let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     //headers.set('Authorization', localStorage.getItem('auth_token'));
     let options = { headers: headers };
-
-
     return this.http.get<Ticket[]>(this.URL + id, options);
+  }
+
+  submitForm(data: any, count: number, tripid: number, userid: number): Observable<any> {
+    console.log(data);
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    headers.set('Authorization', localStorage.getItem('auth_token'));
+    let options = { headers: headers, responseType:'text' as 'json'};
+    return this.http.post<any>(GlobalRootURL.BASE_API_URL + 'trips/'+userid +'/'+tripid+'/buy?count='+count, data, options);
   }
 }
