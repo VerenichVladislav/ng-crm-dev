2a40e330120c9c8533d84b4460c7f4afa8d585e6
OkhremukAlexei <alexei.okhremuk@gmail.com>
2019-11-20 17:13:57 +0300

    fix

--- src/app/core/auth/components/register/register.component.ts
+++ src/app/core/auth/components/register/register.component.ts
@@ -43,7 +43,7 @@ export class RegisterComponent implements OnInit {
             .subscribe(
               (loginResp: Response) => {
                 localStorage.setItem("auth_token", loginResp.headers.get('Authorization'));
-                localStorage.setItem("user", JSON.stringify(new User(resp.body)));
+                localStorage.setItem("user", JSON.stringify(new User(<User>resp.body)));
 
                 this.confirmService.confirmEmail(userData.userName);
 
