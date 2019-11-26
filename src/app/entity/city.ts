4298e07c905537b869a4249f4d39189ec89f341d
Eugenie Chupris <zhenuh15@gmail.com>
2019-11-26 13:57:35 +0300

    add city card on hotelIndex

--- src/app/entity/city.ts
+++ src/app/entity/city.ts
@@ -3,11 +3,13 @@ export class City {
   country: string;
   foundationDate: number;
   population: number;
+  image:string;
 
   constructor(city: City) {
     this.cityName = city.cityName;
     this.country = city.country;
     this.foundationDate = city.foundationDate;
     this.population = city.population;
+    this.image=this.image;
   }
 }
