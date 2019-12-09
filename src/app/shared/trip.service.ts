2713db18425cebfd67a4fea1447fd93376e137b1
Egor Kamysh <egorkamysh99@gmail.com>
2019-12-10 00:47:35 +0300

    Improve buy ticket and replenishing wallet (

--- src/app/shared/trip.service.ts
+++ src/app/shared/trip.service.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {GlobalRootURL} from '../GlobalRootURL';
 import {TripFilters} from '../entity/TripFilters'
@@ -8,14 +7,25 @@ import {TripFilters} from '../entity/TripFilters'
   providedIn: 'root'
 })
 export class TripService {
- readonly ROOT_URL = GlobalRootURL.BASE_API_URL + 'trips/';
+  city:any;
+  readonly ROOT_URL = GlobalRootURL.BASE_API_URL + 'trips/';
   constructor(private http: HttpClient) {}
   tripFilter: TripFilters;
   public setTripFilter(tripFilter:TripFilters){
     console.log(tripFilter.cityFrom);
     this.tripFilter = tripFilter;
   }
-  getTrip(id): any {
-    return this.http.get(this.ROOT_URL + id);
+  
+  getCityName(id:any):any {
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    headers.set('Authorization', localStorage.getItem('auth_token'));
+    let options = { headers: headers, responseType:'text' as 'json'};
+    return this.http.get<any>(GlobalRootURL.BASE_API_URL + 'cities/name/' + id, options)
+    }
+    getTransportName(id:any):any {
+      let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      headers.set('Authorization', localStorage.getItem('auth_token'));
+      let options = { headers: headers, responseType:'text' as 'json'};
+      return this.http.get<any>(GlobalRootURL.BASE_API_URL + 'transports/name/' + id, options)
+      }
   }
-}
