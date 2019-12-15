3a2ee88d984a48a4214ddc85264f59ff1c12e46f
OkhremukAlexei <alexei.okhremuk@gmail.com>
2019-12-15 13:13:29 +0300

    some fixes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,7 +1,6 @@
-import {Component, DoCheck, Input, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { User } from 'src/app/entity/user';
 import {DataTransferService} from '../../shared/data-transfer.service';
-import {Trip} from '../../entity/trip';
 import {Ticket} from '../../entity/ticket';
 import {Reservation} from '../../entity/reservation';
 import {NavigationEnd, Router} from '@angular/router';
@@ -13,10 +12,9 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy{
   private user: User;
   private reservations: Reservation[] = [];
-  private tours;
   private tickets: Ticket[] = [];
   constructor(private transfer: DataTransferService,
               private router: Router,
@@ -43,19 +41,12 @@ export class ProfileComponent {
         this.tickets = this.user.tickets;
 
         this.spinnerService.hide();
-        // if (this.transfer.reservations$.getValue() !== undefined) {
-        //   this.transfer.reservations$.subscribe(value => {
-        //     this.reservations = value;
-        //     if (this.reservations.length > 0) {
-        //       localStorage.setItem('reservations', JSON.stringify(this.reservations));
-        //     }
-        //   });
-        // } else {
-        //   this.reservations = JSON.parse(localStorage.getItem('reservations'));
-        // }
       });
   }
 
+  ngOnInit() {}
+
+  ngOnDestroy() {}
 
   isLater(date: string): boolean {
     return new Date(date) > new Date();
